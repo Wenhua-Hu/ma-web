493d333fe0a5fd3ba47365c90464ccc28d08d05c
WenhuaHu1224 <w.hu1224@gmail.com>
2015-09-03 15:25:57 +0200

    fix: add vector service

--- ma-web/app/js/controllers/controllers.js
+++ ma-web/app/js/controllers/controllers.js
@@ -28,102 +28,34 @@ controller('BackgroundCtrl', function($scope) {
 	$scope.selectedDirection = 'up';
 
 }).
-controller('MapCtrl', function($scope, $window, layerService, geometryService) {
-	$scope.geomCategories = geometryService.getGeometryCategories();
+controller('MapCtrl', function($scope, $window, layerService, geometryService, vectorService) {
 
-	var features = new ol.Collection();
-	var featureOverlay = new ol.layer.Vector({
-		source: new ol.source.Vector({
-			features: features
-		}),
-		style: new ol.style.Style({
-			fill: new ol.style.Fill({
-				color: 'rgba(255, 255, 255, 0.2)'
-			}),
-			stroke: new ol.style.Stroke({
-				color: '#ffcc33',
-				width: 2
-			}),
-			image: new ol.style.Circle({
-				radius: 7,
-				fill: new ol.style.Fill({
-					color: '#ffcc33'
-				})
-			})
-		})
-	});
-
-	var source = new ol.source.Vector({
-		wrapX: false
-	});
-
-	var vector = new ol.layer.Vector({
-		source: source,
-
-		style: new ol.style.Style({
-			fill: new ol.style.Fill({
-				color: 'rgba(255, 255, 255, 0.2)'
-			}),
-			stroke: new ol.style.Stroke({
-				color: '#ffcc33',
-				width: 2
-			}),
-			image: new ol.style.Circle({
-				radius: 7,
-				fill: new ol.style.Fill({
-					color: '#ffcc33'
-				})
-			})
-		})
-	});
-	var mapRast = layerService.init();
-	mapRast.addLayer(vector);
-
-
-	featureOverlay.setMap(mapRast);
-
-
-
-	var modify = new ol.interaction.Modify({
-		features: features,
-
-	});
 	var draw, geometryFunction, maxPoints;
 
-	mapRast.addInteraction(modify);
+	$scope.geomCategories = geometryService.getGeometryCategories();
+	$scope.vector = vectorService.addVector();
+	$scope.mapSource = vectorService.addSource();
 
+	var mapViewer = layerService.init();
+	mapViewer.addLayer($scope.vector);
 
 	$scope.addInteraction = function(geomId) {
-		$scope.removeInteraction();
 
+		//	$scope.removeInteraction();
 		draw = new ol.interaction.Draw({
-			features: features,
-			source: source,
+			source: $scope.mapSource,
 			type: geomId,
 			geometryFunction: geometryFunction,
 			maxPoints: maxPoints
 		});
-		mapRast.addInteraction(draw);
+		mapViewer.addInteraction(draw);
 	}
 
-	$scope.removeInteraction = function() {
-		if (draw != null) {
-			mapRast.removeInteraction(draw);
-		}
-	}
-	$scope.initalDraw = function(draw) {
-			draw = new ol.interaction.Draw({
-				features: features,
-				source: source,
-				type: geomId,
-				geometryFunction: geometryFunction,
-				maxPoints: maxPoints
-			});
-			return draw;
-		}
-		// $scope.initalModify= function(){
-		// 	var modify = new ol.interaction.Modify({
-		//  			features: features,
-		// 	});
-		// }
+	// $scope.removeInteraction = function() {
+	// 	if (draw != null) {
+	// 		mapViewer.removeInteraction(draw);
+	// 	}
+	// }
+	//});
+
 });
--- ma-web/app/js/services.js
+++ ma-web/app/js/services.js
@@ -10,117 +10,161 @@ angular.module('ma-app.services', [
 ]).
 factory('layerService', function() {
 
-  var service = {};
+    var service = {};
 
-  var projection = {};
-  projection['EPSG:28992'] = ol.proj.get('EPSG:28992');
-  projection['EPSG:28992'].setExtent([0, 300000, 300000, 650000]);
+    var projection = {};
+    projection['EPSG:28992'] = ol.proj.get('EPSG:28992');
+    projection['EPSG:28992'].setExtent([0, 300000, 300000, 650000]);
 
-  var map = new ol.Map({
-    target: 'map',
-    logo: false,
-    controls: [
-      new ol.control.Rotate(),
-      new ol.control.Attribution(),
-      new ol.control.ZoomSlider()
-    ],
-    view: new ol.View({
-      projection: projection['EPSG:28992'],
-      center: [155000, 463000],
-      zoom: 13,
-      minZoom: 3,
-      maxZoom: 15,
-    })
-  });
+    var map = new ol.Map({
+      target: 'map',
+      logo: false,
+      controls: [
+        new ol.control.Rotate(),
+        new ol.control.Attribution(),
+        new ol.control.ZoomSlider()
+      ],
+      view: new ol.View({
+        projection: projection['EPSG:28992'],
+        center: [155000, 463000],
+        zoom: 13,
+        minZoom: 3,
+        maxZoom: 15,
+      })
+    });
+    /**
+     * [pdok WMS layer]
+     * @type {ol}
+     */
+    var pdok = new ol.layer.Tile({
+      name: 'PDOK',
+      source: new ol.source.TileWMS({
+        url: 'http://geodata.nationaalgeoregister.nl/wmsc',
+        params: {
+          LAYERS: 'brtachtergrondkaart',
+          VERSION: '1.1.1'
+        },
+        tileGrid: new ol.tilegrid.TileGrid({
+          origin: [-285401.92, 22598.08],
+          resolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.21],
+          tileSize: 256
+        }),
+        projection: projection['EPSG:28992'],
+        extent: [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999]
+      })
+    });
+    /**
+     * [bag WMS layer]
+     * @type {ol}
+     */
+    var bag = new ol.layer.Tile({
+      name: 'BAG',
+      source: new ol.source.TileWMS({
+        url: 'http://213.206.232.120:8080/geoserver/BAG/wms',
+        params: {
+          LAYERS: 'BAG:pandactueelbestaand',
+          VERSION: '1.1.1'
+        },
+        tileGrid: new ol.tilegrid.TileGrid({
+          origin: [-285401.92, 22598.08],
+          resolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.21],
+          tileSize: 256
+        }),
+        projection: projection['EPSG:28992'],
+        extent: [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999]
+      })
+    });
+    map.addLayer(pdok);
+    map.addLayer(bag);
+
+    service.init = function() {
+      return map;
+    };
+    return service;
+  }).
   /**
-   * [pdok WMS layer]
-   * @type {ol}
+   * [vector layer]
+   * @return {[type]} [description]
    */
-  var pdok = new ol.layer.Tile({
-    name: 'PDOK',
-    source: new ol.source.TileWMS({
-      url: 'http://geodata.nationaalgeoregister.nl/wmsc',
-      params: {
-        LAYERS: 'brtachtergrondkaart',
-        VERSION: '1.1.1'
-      },
-      tileGrid: new ol.tilegrid.TileGrid({
-        origin: [-285401.92, 22598.08],
-        resolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.21],
-        tileSize: 256
-      }),
-      projection: projection['EPSG:28992'],
-      extent: [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999]
-    })
-  });
-  /**
-   * [bag WMS layer]
-   * @type {ol}
-   */
-  var bag = new ol.layer.Tile({
-    name: 'BAG',
-    source: new ol.source.TileWMS({
-      url: 'http://213.206.232.120:8080/geoserver/BAG/wms',
-      params: {
-        LAYERS: 'BAG:pandactueelbestaand',
-        VERSION: '1.1.1'
-      },
-      tileGrid: new ol.tilegrid.TileGrid({
-        origin: [-285401.92, 22598.08],
-        resolutions: [3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.21],
-        tileSize: 256
-      }),
-      projection: projection['EPSG:28992'],
-      extent: [-285401.92, 22598.08, 595401.9199999999, 903401.9199999999]
-    })
-  });
-  map.addLayer(pdok);
-  map.addLayer(bag);
-
-  service.init = function() {
-    return map;
-  };
-  return service;
-}).
 factory('geometryService', function() {
+    var service = {};
+
+    var geometryCategories = [{
+      id: '1',
+      name: 'Point',
+      img: 'point.jpg'
+    }, {
+      id: '2',
+      name: 'LineString',
+      img: 'lineString.jpg'
+    }, {
+      id: '3',
+      name: 'Polygon',
+      img: 'pologon.jpg'
+    }, {
+      id: '4',
+      name: 'Circle',
+      img: 'circle.jpg'
+    }, {
+      id: '5',
+      name: 'Square',
+      img: 'square.jpg'
+    }, {
+      id: '6',
+      name: 'Box',
+      img: 'box.jpg'
+    }, {
+      id: '7',
+      name: 'None',
+      img: 'text.jpg'
+    }, {
+      id: '7',
+      name: 'None',
+      img: 'location.jpg'
+    }, ];
+
+    service.getGeometryCategories = function() {
+      return geometryCategories;
+    };
+    return service;
+  }).
+  /**
+   * [vector service]
+   * @param  {Object} ) {var service [description]
+   * @return {[type]}   [description]
+   */
+factory('vectorService', function() {
+
   var service = {};
+  var source = new ol.source.Vector({
+    wrapX: false,
+  });
 
-  var geometryCategories = [{
-    id: '1',
-    name: 'Point',
-    img: 'point.jpg'
-  }, {
-    id: '2',
-    name: 'LineString',
-    img: 'lineString.jpg'
-  }, {
-    id: '3',
-    name: 'Polygon',
-    img: 'pologon.jpg'
-  }, {
-    id: '4',
-    name: 'Circle',
-    img: 'circle.jpg'
-  }, {
-    id: '5',
-    name: 'Square',
-    img: 'square.jpg'
-  }, {
-    id: '6',
-    name: 'Box',
-    img: 'box.jpg'
-  }, {
-    id: '7',
-    name: 'None',
-    img: 'text.jpg'
-  }, {
-    id: '7',
-    name: 'None',
-    img: 'location.jpg'
-  }, ];
+  var vector = new ol.layer.Vector({
+    source: source,
+    style: new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 255, 255, 0.2)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#ffcc33',
+        width: 2
+      }),
+      image: new ol.style.Circle({
+        radius: 7,
+        fill: new ol.style.Fill({
+          color: '#ffcc33'
+        })
+      })
+    })
+  });
 
-  service.getGeometryCategories = function() {
-    return geometryCategories;
+  return {
+    addVector: function() {
+      return vector;
+    },
+    addSource: function() {
+      return source;
+    }
   };
-  return service;
 });
