8e3a1fb5a69a4fb390888b34177baecbed8a8be7
WenhuaHu1224 <w.hu1224@gmail.com>
2015-10-08 22:05:35 +0200

    fix to address

--- ma-web/app/js/controllers/controllers.js
+++ ma-web/app/js/controllers/controllers.js
@@ -98,185 +98,193 @@ controller('MapCtrl', function($http, $scope, $window, x2js, layerService, geome
 					} else {
 						angular.forEach(AddressList, function(value, key) {
 
-								var coordinate = value.Point.pos["__text"];
-								if (value.Address) {
-									var addressInfo = "";
-									var places;
-									// if (value.Address.Place.length=="undefined") {
+							var coordinate = value.Point.pos["__text"];
 
-									// 	addressInfo=value.Address.Place["__text"];
-									// 	console.log(addressInfo);
-									// 	return;
+							if (value.Address) {
+								var addressInfo = "";
+								var places;
 
-									// }
+								if (value.Address.Place.length == undefined) {
 
-									// angular.forEach(value.Address.Place, function(value, key) {
-									// 	var subPlace = value;
-									// 	addressInfo = " " + subPlace + addressInfo;
-									// });
+									addressInfo = value.Address.Place["__text"];
 
-									// if (value.Address.PostalCode) {
-									// 	var postcode = value.Address.PostalCode["__text"];
-									// 	addressInfo = addressInfo + " " + postcode;
-									// }
 
-									// if (value.Address.StreetAddress) {
-									// 	if (value.Address.StreetAddress.Street) {
-									// 		var street = value.Address.StreetAddress.Street["__text"];
-									// 		addressInfo = addressInfo + " " + street;
-									// 		if (value.Address.StreetAddress.Building) {
-									// 			var number = value.Address.StreetAddress.Building["_number"];
-									// 			addressInfo = addressInfo + " " + number;
-									// 			if (value.Address.StreetAddress.Building["_subdivision"]) {
-									// 				var subdivision = value.Address.StreetAddress.Building["_subdivision"];
-									// 				addressInfo = addressInfo + " " + subdivision;
-									// 			}
-									// 		}
-									// 	}
-									// }
-								});
+								} else {
 
-							console.log(addressInfo);
-						}
+									angular.forEach(value.Address.Place, function(value, key) {
+										var subPlace = value;
+										addressInfo = " " + subPlace + addressInfo;
+									});
+
+									if (value.Address.PostalCode) {
+										var postcode = value.Address.PostalCode["__text"];
+										addressInfo = addressInfo + " " + postcode;
+									}
+
+									if (value.Address.StreetAddress) {
+										if (value.Address.StreetAddress.Street) {
+											var street = value.Address.StreetAddress.Street["__text"];
+											addressInfo = addressInfo + " " + street;
+											if (value.Address.StreetAddress.Building) {
+												var number = value.Address.StreetAddress.Building["_number"];
+												addressInfo = addressInfo + " " + number;
+												if (value.Address.StreetAddress.Building["_subdivision"]) {
+													var subdivision = value.Address.StreetAddress.Building["_subdivision"];
+													addressInfo = addressInfo + " " + subdivision;
+												}
+											}
+										}
+									}
+
+								}
+
+								console.log(addressInfo);
+							}
+
+
+						});
 
 					}
 					// map.getView().setCenter(ol.proj.transform([n1, n2], 'EPSG:28992', 'EPSG:28992'));
-
 				},
+
+
 				function() {
 
 				});
+
 		}
-	}
-};
 
-function AddressParse(data) {
-	var response = x2js
-		.xml_str2json(data.data)
-		.GeocodeResponse
-		.GeocodeResponseList;
-	if (response) {
-		var arrayList;
-		var numberOfAddress = response['_numberOfGeocodedAddresses'];
-		if (numberOfAddress == 1) {
+	};
 
+	function AddressParse(data) {
+		var response = x2js
+			.xml_str2json(data.data)
+			.GeocodeResponse
+			.GeocodeResponseList;
+		if (response) {
+			var arrayList;
+			var numberOfAddress = response['_numberOfGeocodedAddresses'];
+			if (numberOfAddress == 1) {
+
+				arrayList = [];
+				arrayList.push(response.GeocodedAddress);
+				//console.log("good" + arrayList);
+			} else {
+				arrayList = response.GeocodedAddress;
+			}
+			return arrayList;
+		} else {
 			arrayList = [];
-			arrayList.push(response.GeocodedAddress);
-			console.log("good" + arrayList);
-		} else {
-			arrayList = response.GeocodedAddress;
+			return arrayList;
 		}
-		return arrayList;
-	} else {
-		arrayList = [];
-		return arrayList;
 	}
-}
 
 
 
-//END OF SEARCH PLAC3E
+	//END OF SEARCH PLAC3E
 
 
 
-var geometryFunction, maxPoints;
-var drawInit, modifyInit;
+	var geometryFunction, maxPoints;
+	var drawInit, modifyInit;
 
-var draw = {
-	init: function(geomId) {
-		this.geometry = new ol.interaction.Draw({
-			features: features,
-			source: mapSource,
-			type: geomId,
-			geometryFunction: geometryFunction
-		});
-		removeInteraction();
-		map.addInteraction(this.geometry);
-		return this.geometry;
-	},
-	setActive: function(active) {
-		//modifyInit.setActive(!active);
-		drawInit.setActive(active);
-		snap.init();
-	}
-};
-
-var select = {
-	init: function() {
-		this.selection = new ol.interaction.Select();
-		map.addInteraction(this.selection);
-
-		return this.selection;
-	}
-};
-
-var modify = {
-	init: function(features) {
-		this.modification = new ol.interaction.Modify({
-			features: features
-		});
-		map.addInteraction(this.modification);
-		return this.modification;
-	},
-	setActive: function(active) {
-		drawInit.setActive(!active);
-		modifyInit.setActive(active);
-		snap.init();
-	}
-};
-
-var snap = {
-	init: function() {
-		this.snaping = new ol.interaction.Snap({
-			source: vector.getSource()
-		});
-		map.addInteraction(this.snaping);
-		return this.snaping;
-	}
-};
-
-var removeInteraction = function() {
-	if (draw !== null) {
-		map.removeInteraction(drawInit);
-	}
-};
-
-$scope.addInteraction = function(geomId) {
-
-
-	if (geomId !== null) {
-		if (geomId === "modify") {
-			var selectOption = select.init();
-			var features = selectOption.getFeatures();
-			modifyInit = modify.init(features);
-			modify.setActive(true);
-
-		} else if (geomId === "move") {
-
-		} else {
-			drawInit = draw.init(geomId);
-			draw.setActive(true);
-
+	var draw = {
+		init: function(geomId) {
+			this.geometry = new ol.interaction.Draw({
+				features: features,
+				source: mapSource,
+				type: geomId,
+				geometryFunction: geometryFunction
+			});
+			removeInteraction();
+			map.addInteraction(this.geometry);
+			return this.geometry;
+		},
+		setActive: function(active) {
+			//modifyInit.setActive(!active);
+			drawInit.setActive(active);
+			snap.init();
 		}
-	} else {}
-	//snap.init();
-};
+	};
+
+	var select = {
+		init: function() {
+			this.selection = new ol.interaction.Select();
+			map.addInteraction(this.selection);
+
+			return this.selection;
+		}
+	};
+
+	var modify = {
+		init: function(features) {
+			this.modification = new ol.interaction.Modify({
+				features: features
+			});
+			map.addInteraction(this.modification);
+			return this.modification;
+		},
+		setActive: function(active) {
+			drawInit.setActive(!active);
+			modifyInit.setActive(active);
+			snap.init();
+		}
+	};
+
+	var snap = {
+		init: function() {
+			this.snaping = new ol.interaction.Snap({
+				source: vector.getSource()
+			});
+			map.addInteraction(this.snaping);
+			return this.snaping;
+		}
+	};
+
+	var removeInteraction = function() {
+		if (draw !== null) {
+			map.removeInteraction(drawInit);
+		}
+	};
+
+	$scope.addInteraction = function(geomId) {
 
 
-//get layers
-var service = layerService; $scope.layersOfMap = [];
+		if (geomId !== null) {
+			if (geomId === "modify") {
+				var selectOption = select.init();
+				var features = selectOption.getFeatures();
+				modifyInit = modify.init(features);
+				modify.setActive(true);
 
-$scope.addLayers = function() {
-	angular.forEach(service.map().getLayers(), function(layer, key) {
-		var temp = {
-			"LayerName": layer.getSource(),
-			"Visibility": layer.getVisible()
-		};
-		$scope.layersOfMap.push(temp);
-	});
-	return $scope.layersOfMap;
-};
-//add marker
+			} else if (geomId === "move") {
+
+			} else {
+				drawInit = draw.init(geomId);
+				draw.setActive(true);
+
+			}
+		} else {}
+		//snap.init();
+	};
+
+
+	//get layers
+	var service = layerService;
+	$scope.layersOfMap = [];
+
+	$scope.addLayers = function() {
+		angular.forEach(service.map().getLayers(), function(layer, key) {
+			var temp = {
+				"LayerName": layer.getSource(),
+				"Visibility": layer.getVisible()
+			};
+			$scope.layersOfMap.push(temp);
+		});
+		return $scope.layersOfMap;
+	};
+	//add marker
 }).controller('testController', function($scope, $rootScope, layerService) {
 	// TestService.getResult().then(function(success) {
 	// 	//$scope.showResult = success.data;
